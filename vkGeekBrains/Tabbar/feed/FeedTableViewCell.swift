9e80e363db7b1de377d2447c1f878aedd1363f16
far666 <far.brest666@gmail.com>
2022-12-11 11:07:36 +0300

    check

---
+++ vkGeekBrains/Tabbar/feed/FeedTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedTableViewCell.swift
+//  vkGeekBrains
+//
+//  Created by Aleksandr on 9.12.22.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var postName: UILabel!
+    @IBOutlet weak var postData: UILabel!
+    @IBOutlet weak var postText: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    
+}
--- vkGeekBrains/Tabbar/feed/FeedViewController.swift
+++ vkGeekBrains/Tabbar/feed/FeedViewController.swift
@@ -7,23 +7,48 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet weak var table: UITableView!
+     
+    var posts: [Post] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        APIManager.shared.getPost(id: "post1", imageID: "1") {res1 in
+            self.posts.append(res1!)
+            APIManager.shared.getPost(id: "post2", imageID: "2") {res2 in
+                self.posts.append(res2!)
+                APIManager.shared.getPost(id: "post3", imageID: "3") {res3 in
+                    self.posts.append(res3!)
+                    APIManager.shared.getPost(id: "post4", imageID: "4") {res4 in
+                        self.posts.append(res4!)
+                        APIManager.shared.getPost(id: "post5", imageID: "5") {res5 in
+                            self.posts.append(res5!)
+                            DispatchQueue.main.async {
+                                self.table.reloadData() 
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedTableViewCell
+        cell.postName.text = posts[indexPath.row].name
+        cell.postData.text = posts[indexPath.row].data
+        cell.postText.text = posts[indexPath.row].text
+        cell.postImage.image = posts[indexPath.row].image
+        return cell
+    }
+    
+    
 }
